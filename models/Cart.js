644b55d0ed5a6a9f1af2d119faba485fcc96dd72
accelerator-01 <acceleratorgame01@gmail.com>
2023-12-23 10:21:55 +0700

    Menambah component Cart

---
+++ models/Cart.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const cartSchema = new Schema({
+  userId: {
+    type: String,
+    ref: "user",
+  },
+  items: [
+    {
+      productId: {
+        type: String,
+        ref: "item",
+      },
+      name: String,
+      quantity: {
+        type: Number,
+        required: true,
+        min: [1, "Quantity can not be less then 1."],
+        default: 1,
+      },
+      price: Number,
+    },
+  ],
+  bill: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+const Cart = mongoose.model("cart", cartSchema);
+
+export default Cart;

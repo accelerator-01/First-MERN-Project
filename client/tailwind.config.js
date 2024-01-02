3d60ce90fa75aa4edce94d388f365205a8d44b94
accelerator-01 <acceleratorgame01@gmail.com>
2024-01-02 21:25:38 +0700

    Mencoba menggunakan component library

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     fontFamily: {
       mons: "Montserrat",
       sans: "Roboto mono",
     },
-    extend: {
-      height: {
-        screen: "100dvh",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+});
